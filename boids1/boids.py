98a33105f6320864b81978de6ad98059bd403ef0
thegrandmother <henrik.sommerland@gmail.com>
2016-04-11 17:41:27 +0200

    latest coolness

--- boids1/boids.py
+++ boids1/boids.py
@@ -1,7 +1,6 @@
-
 import os, sys, pygame
 from pygame.locals import *
-from math import pi
+from math import pi,sqrt,sin,cos,asin,atan
 
 
 WHITE = 255,255,255
@@ -10,26 +9,59 @@ BLACK = 0,0,0
 BLUE  = 0,0,255
 RED   = 255,0,0
 
-vel_scale = 1
+vel_scale = 5
 pos_scale = 1
 pos_offs = 0
 
+poly = [
+        (0,1),(2/3.0,-2/3.0),(0,1/3.0),(-2/3.0,-2/3.0),(0,1)
+        ]
 
-def drawBoid((pos,vel)):
+def normalize((x,y)):
+    magn = sqrt(x**2+y**2)
+    return (x/magn, y/magn)
+
+def rotate((x,y),a):
+    return (x*cos(a)-y*sin(a), x*sin(a)+y*cos(a))
+
+def sign(x) :
+	if x < 0:
+		return -1
+	else:
+		return 1
+
+def makeBoidPolly((pos,vel)):
     (x,y) = pos
-    c_pos = (x*pos_scale + pos_offs,x*pos_scale + pos_offs)
+    (dx,dy) = normalize(vel)
+    angle = (asin(dx)-pi)
+    scale = 10
+    rotated = map(lambda c : rotate(c,angle),poly)
+    offsetted = map(lambda (xx,yy) : ((x+xx*scale),(y+yy*scale)),rotated)
+    return offsetted
+
+def drawBoid((pos,vel),color):
+    (x,y) = pos
+    c_pos = (x*pos_scale + pos_offs,y*pos_scale + pos_offs)
     (cx,cy) = c_pos
     (vx,vy) = vel
     c_vel = (cx + vx*vel_scale,cy + vy*vel_scale)
 
-    pygame.draw.line(
-            screen, WHITE, c_pos, 
-            c_vel, 
+    pygame.draw.polygon(
+            screen, color,  makeBoidPolly((pos,vel)), 
             1)
+
+def lameDraw(((x,y),(vx,vy)),index):
+	(cx,cy) = (x*pos_scale + pos_offs,y*pos_scale + pos_offs)
+	(cvx,cvy) = normalize((vx,vy))
+	(cvx,cvy) = (cx+cvx*10,cy+cvy*10)
+	pygame.draw.line(
+	        screen, index, (cx,cy), 
+	        (cvx,cvy), 
+	        4)
 
 def clearBoid((pos,vel)):
     (x,y) = pos
-    c_pos = (x*pos_scale + pos_offs,x*pos_scale + pos_offs)
+    c_pos = (x*pos_scale + pos_offs,y*pos_scale + pos_offs)
     (cx,cy) = c_pos
     (vx,vy) = vel
     c_vel = (cx + vx*vel_scale,cy + vy*vel_scale)
@@ -61,11 +93,14 @@ if __name__ == "__main__":
     line = boid_file.readline()
     while 1:
         if line != "":
-            (index,data) = buidBoid(line)
-            if index in boids:
-                clearBoid(boids[index])
-            boids[index] = data
-            drawBoid(data)
+						if(len(line.split(",")) == 5):
+							(index,data) = buidBoid(line)
+							if index in boids:
+									#drawBoid(boids[index],BLACK)
+									lameDraw(boids[index],BLACK)
+							#boids[index] = data
+							lameDraw(data,WHITE)
+							drawBoid(data,WHITE)
             
         line = boid_file.readline()
         for event in pygame.event.get():
@@ -76,4 +111,4 @@ if __name__ == "__main__":
                 pygame.display.quit()
                 sys.exit(0)
         pygame.display.update() 
-        pygame.time.delay(20)
+        pygame.time.delay(10)
