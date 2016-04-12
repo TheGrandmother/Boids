2973d21c0f7453beb7c9ffabb763a55d753cd9c2
thegrandmother <henrik.sommerland@gmail.com>
2016-04-12 15:53:52 +0200

    Tringle graphics

--- boids1/boids.py
+++ boids1/boids.py
@@ -12,20 +12,50 @@ RED   = 255,0,0
 vel_scale = 5
 pos_scale = 1
 pos_offs = 0
+poly_scale = 10;
+poly = [
+        (1,0),
+        (0,1/3.0),
+        (0,-1/3.0),
+        (1,0)
+        ]
+
+def rotate((x,y),a):
+    return (x*cos(a)-y*sin(a), x*sin(a)+y*cos(a))
+
+def sign(x):
+    if(x < 0):
+        return -1
+    else:
+        return 1
 
 def normalize((x,y)):
     magn = sqrt(x**2 + y**2)
     return (x/magn,y/magn)
 
-def drawBoid(((x,y),(vx,vy)),index):
-        (cx,cy) = (x*pos_scale + pos_offs,y*pos_scale + pos_offs)
-        (cvx,cvy) = normalize((vx,vy))
-        (cvx,cvy) = (cx+cvx*10,cy+cvy*10)
+def computeAngle((dx,dy)):
+    angle = atan(dy/dx)
+    if (dx < 0):
+      angle += pi
+    return angle
 
-        pygame.draw.aaline(
-                screen, index, (cx,cy), 
-                (cvx,cvy), 
-                4)
+def makeBoidPolly((pos,vel)):
+    (x,y) = pos
+    (dx,dy) = normalize(vel)
+    angle = computeAngle((dx,dy)) 
+    rotated = map(lambda c : rotate(c,angle),poly)
+    offsetted = map(lambda (xx,yy) : ((x+xx*poly_scale),(y+yy*poly_scale)),rotated)
+    return offsetted
+
+def drawBoid(((x,y),(vx,vy)),index):
+
+        pygame.draw.lines(
+                screen, index,False, makeBoidPolly(((x,y),(vx,vy))),1)
+
+def clearBoid(((x,y),(vx,vy))):
+
+    pygame.draw.lines(
+            screen, BLACK,True, makeBoidPolly(((x,y),(vx,vy))),1)
 
 def buidBoid(line):
     line = line.replace("\n","")
@@ -47,12 +77,13 @@ if __name__ == "__main__":
 
     boid_file = open("test.txt","r");
     line = raw_input()
+    counter = 0
     while 1:
         if line != "":
             if(len(line.split(",")) == 5):
                 (index,data) = buidBoid(line)
                 if index in boids:
-                    drawBoid(boids[index],BLACK)
+                    clearBoid(boids[index])
                 boids[index] = data
                 drawBoid(data,WHITE)
         line = raw_input()
@@ -66,4 +97,6 @@ if __name__ == "__main__":
                 sys.exit(0)
 
         pygame.display.update() 
-        pygame.time.delay(1)
+        pygame.time.delay(0)
+        counter += 1;
+
